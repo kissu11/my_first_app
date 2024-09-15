44bc5901cdc28407889d42579d530d2131e06ea9
Krishnakant Thakur <kissu6440@gmail.com>
2024-09-16 01:25:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
  const app=express();
+ app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+  });
  app.get('/',(req,res)=>{
     res.send("Home page");
  })
